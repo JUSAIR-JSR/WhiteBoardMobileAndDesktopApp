a52b8d4d038c07ea16f73c0ce54828b23e65a66e
JUSAIR <141427676+JUSAIR-JSR@users.noreply.github.com>
2025-11-27 08:56:15 +0530

    Update vite.config.js

--- desktop/vite.config.js
+++ desktop/vite.config.js
@@ -1,10 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    host: true,
     port: 5173,
-    host: true
   },
+
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+  },
 });
